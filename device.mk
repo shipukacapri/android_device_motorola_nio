7f7cf4495ffa3ef0f308f90293507793b331e5fa
Shipu <242039583+shipukacapri@users.noreply.github.com>
2026-02-15 11:28:54 +0530

    adding infinity properties

--- device.mk
+++ device.mk
@@ -43,5 +43,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/nfc/libnfc-nci.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nci.conf \
     $(LOCAL_PATH)/nfc/libnfc-nxp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nxp.conf
 
+# Infinity Specific Properties
+PRODUCT_SYSTEM_PROPERTIES += \
+    ro.infinity.soc=Snapdragon_870 \
+    ro.infinity.camera=64MP+16MP+2MP+16MP+8MP
+
 # Get non-open-source specific aspects
 $(call inherit-product-if-exists, vendor/motorola/nio/nio-vendor.mk)
