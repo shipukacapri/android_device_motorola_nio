a4888d462711a3e5557c41b5a8ac3956246fa4ff
Shipu <242039583+shipukacapri@users.noreply.github.com>
2026-04-18 06:57:20 +0530

    nio: Lunaris BringUp

--- lineage_nio.mk
+++ lineage_nio.mk
@@ -12,6 +12,10 @@ PRODUCT_NAME := lineage_nio
 PRODUCT_DEVICE := nio
 PRODUCT_MODEL := XT2125-4
 
+# Lunaris-Flags
+WITH_GMS := true
+WITH_BCR := true
+
 # Build info
 PRODUCT_BUILD_PROP_OVERRIDES += \
     BuildDesc="nio_retcn-user 12 S1RN32.55-16-13 fce21a-b9f4e9 release-keys" \
