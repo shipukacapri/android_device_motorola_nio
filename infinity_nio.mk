64bbac82c447302ce2c341fb690faeed63f373d2
Shipu <242039583+shipukacapri@users.noreply.github.com>
2026-05-14 18:02:56 +0530

    nio: InfinityX bringup (makefile)

--- infinity_nio.mk
+++ infinity_nio.mk
@@ -14,3 +14,10 @@ PRODUCT_MODEL := Moto G100
 
 # Infinity Flags
 INFINITY_MAINTAINER := Shipu
+
+# Build info
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BuildDesc="nio_retcn-user 12 S1RN32.55-16-13 fce21a-b9f4e9 release-keys" \
+    BuildFingerprint=motorola/nio_retcn/nio:12/S1RN32.55-16-13/fce21a-b9f4e9:user/release-keys \
+    DeviceProduct=nio_retcn \
+    SystemName=nio_retcn
